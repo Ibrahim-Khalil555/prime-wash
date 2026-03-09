0ba7eb7a61ff0e54f5e0d502fdd1e0d1074b19f4
Ibrahim-Khalil555 <khalilmdibrahim752@gmail.com>
2026-03-09 17:08:40 +0600

    fix banner bg

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,12 @@ import Button from "../layouts/Button";
 
 const Hero = () => {
   return (
-    <div
-      aria-label="A view of the mountains at sunrise"
-      className=" min-h-screen flex flex-row justify-between items-center lg:px-32 px-5 bg-[url('')] bg-cover bg-no-repeat "
-    >
+    <div className=" min-h-screen flex flex-row justify-between items-center lg:px-32 px-5">
+      <img
+        src="./assets/img/banner.jpg"
+        className=" bg-cover bg-no-repeat "
+        alt=""
+      />
       <div className=" w-full lg:w-2/3 space-y-5">
         <h1 className=" text-white font-semibold text-6xl">
           Premium Care for Every Drive.
