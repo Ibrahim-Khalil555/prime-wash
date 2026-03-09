bd8f1e3bd85b7f0eb50c8c3c78f1f18753d25411
Ibrahim-Khalil555 <khalilmdibrahim752@gmail.com>
2026-03-09 17:16:55 +0600

    fix banner

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,13 +2,8 @@ import Button from "../layouts/Button";
 
 const Hero = () => {
   return (
-    <div className="relative h-64 w-full overflow-hidden">
-      <img
-        src="./assets/img/banner.jpg"
-        className=" absolute inset-0 w-full h-full object-cover bg-no-repeat "
-        alt="hero image"
-      />
-      <div className=" relative z-10 flex items-center justify-center h-full">
+    <div className="min-h-screen flex flex-row justify-between items-center lg:px-32 px-5 bg-[url('./assets/img/banner.jpg')] bg-cover bg-no-repeat ">
+      <div className=" w-full lg:w-2/3 space-y-5">
         <h1 className=" text-white font-semibold text-6xl">
           Premium Care for Every Drive.
         </h1>
