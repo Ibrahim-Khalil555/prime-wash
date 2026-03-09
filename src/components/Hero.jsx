853b36b3327b479730ade8790be292608aa76ae8
Ibrahim-Khalil555 <khalilmdibrahim752@gmail.com>
2026-03-09 16:58:04 +0600

    remove hero img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,10 @@ import Button from "../layouts/Button";
 
 const Hero = () => {
   return (
-    <div className=" min-h-screen flex flex-row justify-between items-center lg:px-32 px-5 bg-[url('./assets/img/banner.jpg')] bg-cover bg-no-repeat ">
+    <div
+      aria-label="A view of the mountains at sunrise"
+      className=" min-h-screen flex flex-row justify-between items-center lg:px-32 px-5 bg-[url('')] bg-cover bg-no-repeat "
+    >
       <div className=" w-full lg:w-2/3 space-y-5">
         <h1 className=" text-white font-semibold text-6xl">
           Premium Care for Every Drive.
