90147e7dc3d30a5e5dbec6774a91d9ab283f1c0d
Ibrahim-Khalil555 <khalilmdibrahim752@gmail.com>
2026-03-09 16:33:17 +0600

    Fix

--- src/layouts/ServicesCard.jsx
+++ src/layouts/ServicesCard.jsx
@@ -5,7 +5,7 @@ import Button from "./Button";
 const ServicesCard = (props) => {
   return (
     <div className=" w-full lg:w-1/4 p-5 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] rounded-lg">
-      <img className=" rounded-xl " src={props.img} alt="img" />
+      <img className=" rounded-xl " src={props.img} alt={props.title} />
       <div className=" space-y-4">
         <h3 className=" font-semibold text-center text-xl pt-6">
           {props.title}
